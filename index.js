397c3c475fd2babcf1d6e5f97f8eb6bc22dedbd1
Forhad <farhadhossain01992@gmail.com>
2022-11-25 01:19:49 +0600

    categories data commited

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,11 +11,37 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vjjynh2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const categoriesCollection = client.db('furnitureZone').collection('categories');
+
+        app.get('/categoryName' , async(req, res) =>{
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        });
+
+
+    }
+    finally{
+
+    }
+
+
+}
+run().catch(console.log);
+
+
+
+
 app.get('/', async (req, res) => {
     res.send('Used server is running');
 });
 
-app.listen(port, () => console.log(`Used server also runnig running on ${port}`))
+app.listen(port, () => console.log(`Used server also running running on ${port}`))
 
 
 
